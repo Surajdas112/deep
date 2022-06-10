4afc99f4d4cd95e9102aa7bd32560092ff335f25
suraj <surajdas112@gmail.com>
2022-06-10 21:05:53 +0530

    Initial commit

--- Main.java
+++ Main.java
@@ -1,2 +1,13 @@
+<<<<<<< HEAD
 package PACKAGE_NAME;public class Main {
 }
+=======
+public class Main {
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
+
+>>>>>>> f57884a (Initial commit)
